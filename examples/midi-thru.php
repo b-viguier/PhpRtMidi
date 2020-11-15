13f7476a7c7d26118d191fea4233e1358d3ed3b4
Benoit Viguier <benoit.viguier.pro+github@gmail.com>
2020-11-15 15:58:03 +0100

    feat(example): Midi thru

---
+++ examples/midi-thru.php
@@ -0,0 +1,35 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+function select(string $title, array $list): string
+{
+    do {
+        echo "\n===============\n";
+        $list = array_values($list);
+        foreach ($list as $key => $value) {
+            echo "[$key] $value\n";
+        }
+        echo "$title\n?> ";
+        $choice = fgetc(\STDIN);
+    } while (!isset($list[$choice]));
+
+    return $list[$choice];
+}
+
+$browser = new \bviguier\RtMidi\MidiBrowser();
+
+$input = $browser->openInput(select("Select a MIDI input", $browser->availableInputs()));
+$output = $browser->openOutput(select("Select a MIDI output", $browser->availableOutputs()));
+
+echo "Midi thru enabled, use Ctr-C to exit…\n";
+$msgCount = 0;
+while (true) {
+    if ($msg = $input->pullMessage()) {
+        $mem = memory_get_usage();
+        ++$msgCount;
+        echo "\r$msgCount messages transferred (Memory $mem) ";
+        $output->send($msg);
+    }
+    usleep(100);
+}
